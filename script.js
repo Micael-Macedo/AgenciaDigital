53f1f55c187140a363b24ec25457f449a75ebbc7
Micael-Macedo <micaelm2009@hotmail.com>
2023-03-13 11:52:01 -0300

    feat: add project

---
+++ script.js
@@ -0,0 +1,14 @@
+function openDropdownMenu(option) {
+    let selected = $(option).parents("li").children("ul");
+    $(".desktop").find("a").removeClass("selected");
+    $(".mobile").find("a").removeClass("selected");
+    $(option).addClass("selected");
+    $(".dropdown-menu").removeClass("open");
+    $(selected).toggleClass("open");
+
+}
+$(document).ready(function () {
+});
+function navMobile() {
+    $(".mobileOption").toggleClass("open");
+}
