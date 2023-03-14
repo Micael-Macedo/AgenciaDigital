33e2dda8490a76c1f7ca762d156336e806da32a9
Micael-Macedo <micaelm2009@hotmail.com>
2023-03-14 10:56:20 -0300

    fix: navbar dropdown menu

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 function openDropdownMenu(option) {
-    let selected = $(option).parents("li").children("ul");
-    $(".desktop").find("a").removeClass("selected");
-    $(".mobile").find("a").removeClass("selected");
-    $(option).toggleClass("selected");
+    let selected;
+    if($(option).parents("div").hasClass("mobileOption")){
+        selected = $(option).parents("div.navMenu").children("ul");
+        console.log("Mobile")
+    }
+    else{
+        selected = $(option).parents("li").children("ul");
+        console.log("Desktop", selected)
+    }
     $(".dropdown-menu").removeClass("open");
-    $(selected).toggleClass("open");
-
+    if($(option).hasClass("selected")){
+        $(".desktop").find("a").removeClass("selected");
+        $(".mobile").find("a").removeClass("selected");
+        $(option).removeClass("selected");
+        $(selected).removeClass("open");
+    }else{
+        $(".desktop").find("a").removeClass("selected");
+        console.log($(".mobile").find("a"))
+        $(".mobileOption").find("a").removeClass("selected");
+        $(option).addClass("selected");
+        $(selected).addClass("open");
+    }
 }
-$(document).ready(function () {
-});
 function navMobile() {
     $(".mobileOption").toggleClass("open");
 }
